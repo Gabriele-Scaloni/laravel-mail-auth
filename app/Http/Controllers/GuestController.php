f361816d6a9ed251e33b61a6241eb57f6e3c46d8
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2022-02-14 16:06:43 +0100

    add route home

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class GuestController extends Controller
+
 {
-    //
+    public function home() {
+
+    return view('pages.home');
+
+    }
 }
